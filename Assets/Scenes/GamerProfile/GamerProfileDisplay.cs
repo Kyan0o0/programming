ab009693b9700d8e05a640370e2410e0c79a1592
Kyano <40870@ma-web.nl>
2025-10-07 14:28:51 +0200

    second commit

---
+++ Assets/Scenes/GamerProfile/GamerProfileDisplay.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+public class les2 : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Debug.Log("=== Pian0o0 ===");
+        Debug.Log("Gamer Tag: Piano");
+        Debug.Log("Favorite Game: subnautica");
+        Debug.Log("Gaming Platform: PC");
+        Debug.Log("Hours Played Today: 3");
+        Debug.Log("Current Level: 0");
+        Debug.Log("Achievement Unlocked: First Script!");
+        Debug.Log("==================");
+        Debug.Log("Fav food: Pizza");
+        Debug.Log("Fav anime: Bleach");
+        Debug.Log("most hated game:A hat in time");
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+
+    }
+}
